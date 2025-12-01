18ab5c5d6bddc125be4cfdf183893b202c74b6a3
emma <egujral26@aoiths.org>
2025-12-01 18:58:05 -0500

    did functions with no params

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 // ===== FUNCTIONS WITH NO PARAMETERS ===== //
 
+function sayHello() {
+    document.getElementById("output1").textContent =
+        "Hello, Madison!";
+}
 
+function showDate() {
+    const today = new Date();
+    const dateString = `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
+    document.getElementById("output1").textContent = "Today’s date is " + dateString;
+}
 
+function motivate() {
+    document.getElementById("output1").textContent =
+        "“When I'm around you, I kind of feel like I'm on drugs. Not that I do drugs. Unless you do drugs, in which case I do them all the time. All of them.” -Scott Pilgrim";
+}
 
-
-
-
-
-
-
+function showRep() {
+    document.getElementById("output1").textContent =
+        "I represent me.";
+}
 
 // ===== FUNCTIONS WITH PARAMETERS ===== //
-
-
-
-
-
-
-
-
-
 
 // ===== EVENT LISTENERS THAT CALLS FUNCTIONS ===== //
